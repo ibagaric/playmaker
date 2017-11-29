9d352c638d9aad09cab39d3a218634884a6be10a
Ivan Bagaric <ivan.bagaric@intis.hr>
2017-11-29 15:16:33 +0100

    base for players

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyBYNfjghmdJBao0Lybn2hHiy2rsQ61Aayc",
+    authDomain: "shop-3bd99.firebaseapp.com",
+    databaseURL: "https://shop-3bd99.firebaseio.com",
+    projectId: "shop-3bd99",
+    storageBucket: "shop-3bd99.appspot.com",
+    messagingSenderId: "402917128025"
   }
 };
